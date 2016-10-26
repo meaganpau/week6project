5d09ede5e97ea53d50d3cd5566faa369b9852ac1
Meagan Pau <meagan.pau@gmail.com>
2016-10-26 12:57:41 -0400

    fadein and out of sections

--- dev/scripts/main.js
+++ dev/scripts/main.js
@@ -1,5 +1,8 @@
-var userPrice = '20';
-var userProduct = 'bronzer';
+// var userPriceMin = array[0];
+// var userPriceMax = array[1];
+// var userProduct = product.val();
+// var array = val.split(',');
+
 
 var makeupApp = {};
 makeupApp.getMakeup = function() {
@@ -8,7 +11,8 @@ makeupApp.getMakeup = function() {
 		method: 'GET',
 		dataType: 'json',
 		data: {
-			price_less_than: userPrice,
+			price_greater_than: userPriceMin,
+			price_less_than: userPriceMax,
 			product_type: userProduct
 		}
 	}).then(function(results){
@@ -41,6 +45,29 @@ makeupApp.init = function() {
 	makeupApp.getMakeup();
 }
 
+$('#start').on('click', function(e){
+	e.preventDefault();
+	$('#section1').fadeOut();
+	$('#section2').delay(500).fadeIn();
+})
+
+$('#next1').on('click', function(e){
+	e.preventDefault();
+	$('#section2').fadeOut();
+	$('#section3').delay(500).fadeIn();
+})
+
+$('#next2').on('click', function(e){
+	e.preventDefault();
+	$('#section3').fadeOut();
+	$('#section4').delay(500).fadeIn();
+})
+
+$('#next3').on('click', function(e){
+	e.preventDefault();
+	$('#section4').fadeOut();
+	$('#section5').delay(500).fadeIn();
+})
 
 $(function(){
 	console.log('hey hey');
